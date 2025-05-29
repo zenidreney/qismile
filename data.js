f8bf4abb7e18d2517c52eff3003531ef66f0e764
Deniz <auo@tuta.io>
2025-05-29 22:26:31 +0200

    working on daily quote page

--- data.js
+++ data.js
@@ -2,4 +2,7 @@ export const dailyQuotes = [
     "I am Love",
     "I am Whole",
     "I am Loved",
+    "I am Space",
+    "I am Empty",
+    "I am Compassion",
 ];
--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 import { dailyQuotes } from "/data.js";
 
-console.log(dailyQuotes);
+const quotesPara = document.getElementById("quotes-para");
+const quotesBtn = document.getElementById("quotes-btn");
+const randomNumber = Math.floor(Math.random() * dailyQuotes.length);
+
+quotesBtn.addEventListener("click", renderQuote);
+
+function renderQuote() {
+    quotesPara.innerHTML = `
+                <div id="quotes-para" class="mt-3">
+                
+                    <p class="lead fw-bolder">
+                        ${dailyQuotes[randomNumber]}
+                    </p>
+                    
+                </div>
+                `;
+}
