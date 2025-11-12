eb7783e30f75d85048f0e1b918f8811687ac9802
Deniz <auo@tuta.io>
2025-11-12 19:48:58 +0100

    Worked on layout

--- components/footer.js
+++ components/footer.js
@@ -8,4 +8,13 @@ export async function loadFooter() {
     const footer = doc.getElementById("footer-container")
 
     document.querySelector("footer").append(footer)  
+
+    updateYear()
+}
+
+function updateYear() {
+    const yearSpan = document.getElementById("year-span")
+    const currentYear = new Date().getFullYear()
+
+    yearSpan.textContent = currentYear.toString()
 }
