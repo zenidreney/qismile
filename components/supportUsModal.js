843040b29cd1f9435e271eb7408010bf2dd019dc
Deniz <auo@tuta.io>
2025-11-20 15:03:12 +0100

    corrected semantic html of the modal

--- components/supportUsModal.js
+++ components/supportUsModal.js
@@ -2,26 +2,34 @@
 
 export async function loadSupportUsModal() {
 
-    
+    const modalContainer = document.getElementById("support-us-modal")
+    // console.log(modalContainer)
+
+    if (modalContainer) {
 
 
         const res = await fetch("/components/support-us-modal.html")
         const rawHtmlText = await res.text()
-        console.log(rawHtmlText)
-        
+        // console.log(rawHtmlText)
+
         const parser = new DOMParser()
         const doc = parser.parseFromString(rawHtmlText, "text/html")
-        
-        const modal = doc.getElementById("support-us-modal")
-        console.log(modal)
-    
-        document.body.append(modal)  
-    
-        const supportUsModal = new bootstrap.Modal(modal)
+
+        const modal = doc.querySelector(".modal-dialog")
+        // console.log(modal)
+
+
+        modalContainer.append(modal)
+
+        const supportUsModal = new bootstrap.Modal(modalContainer)
         setTimeout(() => {
             supportUsModal.show()
         }, 8000);
+    }
 
-    
+
+
+
+
 
 }
