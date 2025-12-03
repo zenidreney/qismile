c84c65121ecedd05657195a928c5578c39e31680
Deniz <auo@tuta.io>
2025-12-03 17:43:04 +0100

    code cleaning.

--- scripts.js
+++ scripts.js
@@ -1,37 +1,21 @@
-import { loadNavBar } from "./components/navbar.js";
-
 import { loadFooter } from "./components/footer.js";
-
+import { loadNavBar } from "./components/navbar.js";
 import { renderQuote } from "./utils/renderQuote.js";
-
 import { loadSupportUsModal } from "./components/supportUsModal.js";
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    
-    /*Load Navbar component at the top to header div*/
-    
-    loadNavBar();
-
-    /*Load footer to the footer tag*/
-
-    loadFooter();
-})
-
-/* Some pages have a moda popup */
-
+	loadNavBar();
+	loadFooter();
+});
 
 if (document.getElementById("support-us-modal")) {
-    
-    loadSupportUsModal()
+	loadSupportUsModal();
 }
 
-/*Logic for daily-insipiration page*/
-
 if (document.getElementById("quotes-btn")) {
-    const quotesBtn = document.getElementById("quotes-btn");
-    quotesBtn.addEventListener("click", () => {
-        renderQuote();
-        quotesBtn.disabled = true;
-    });
+	const quotesBtn = document.getElementById("quotes-btn");
+	quotesBtn.addEventListener("click", () => {
+		renderQuote();
+		quotesBtn.disabled = true;
+	});
 }
