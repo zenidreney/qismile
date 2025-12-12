4fe10e219c2762128b62baeba15cfa97bbb00d55
Deniz <auo@tuta.io>
2025-12-12 19:19:15 +0100

    added html message

--- components/supportUsModal.js
+++ components/supportUsModal.js
@@ -2,18 +2,15 @@
 
 export async function loadSupportUsModal() {
 	const modalContainer = document.getElementById("support-us-modal");
-	// console.log(modalContainer)
 
 	if (modalContainer) {
 		const res = await fetch("/components/support-us-modal.html");
 		const rawHtmlText = await res.text();
-		// console.log(rawHtmlText)
 
 		const parser = new DOMParser();
 		const doc = parser.parseFromString(rawHtmlText, "text/html");
 
 		const modal = doc.querySelector(".modal-dialog");
-		// console.log(modal)
 
 		modalContainer.append(modal);
 
